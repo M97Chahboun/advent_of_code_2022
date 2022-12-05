3911d77b36ca556d83e09b9280cf5f3a1e1dc188
Mohammed <m97.chahboun@gmail.com>
2022-12-05 21:58:09 +0100

    Day 4 / Part 2 🙃

--- lib/day_4/day_4.dart
+++ lib/day_4/day_4.dart
@@ -5,8 +5,9 @@ class Day4 {
     print("--- Day 4: Camp Cleanup ---");
     List<String> assignments =
         PrepareData.getData(path: "lib/day_4/day_4.txt", pattern: "\n");
-        
+
     part1(assignments);
+    part2(assignments);
   }
 
   static void part1(List<String> assignments) {
@@ -14,17 +15,35 @@ class Day4 {
     int sum = 0;
     for (var assignment in assignments) {
       List<String> pairs = assignment.split(",");
-      if (isContained(pairs[0], pairs[1]) || isContained(pairs[1], pairs[0])) {
+      if (isContainedOrOverlap(pairs[0], pairs[1]) ||
+          isContainedOrOverlap(pairs[1], pairs[0])) {
         sum += 1;
       }
     }
     print("assignment pairs does one range fully contain the other : $sum");
   }
 
-  static bool isContained(String pairS1, String pairS2) {
+  static bool isContainedOrOverlap(String pairS1, String pairS2,
+      {bool isOverlap = false}) {
     List<int> pair1 = pairS1.split("-").map(int.parse).toList();
     List<int> pair2 = pairS2.split("-").map(int.parse).toList();
+    if (isOverlap) {
+      return !((pair1[1] < pair2[0]) || (pair1[0] > pair2[1]));
+    }
     return pair1[0] >= pair2[0] && pair1[1] <= pair2[1];
+  }
+
+  static part2(List<String> assignments) {
+    print("Part 2:");
+    int sum = 0;
+    for (var assignment in assignments) {
+      List<String> pairs = assignment.split(",");
+      if (isContainedOrOverlap(pairs[0], pairs[1], isOverlap: true) ||
+          isContainedOrOverlap(pairs[1], pairs[0], isOverlap: true)) {
+        sum += 1;
+      }
+    }
+    print("assignment pairs do the ranges overlap : $sum");
   }
 }
 
