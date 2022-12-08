c014bf344b77422a94f771fcd4e0244df4628adf
Mohammed <m97.chahboun@gmail.com>
2022-12-08 22:43:04 +0100

    Day 6 / Part 1 🤨

--- bin/advent_of_code_2022.dart
+++ bin/advent_of_code_2022.dart
@@ -1,6 +1,7 @@
 import 'package:advent_of_code_2022/advent_of_code_2022.dart';
 import 'package:advent_of_code_2022/day_4/day_4.dart';
 import 'package:advent_of_code_2022/day_5/day_5.dart';
+import 'package:advent_of_code_2022/day_6/day_6.dart';
 
 void main(List<String> arguments) {
   Day1.solve();
@@ -8,4 +9,5 @@ void main(List<String> arguments) {
   Day3.solve();
   Day4.solve();
   Day5.solve();
+  Day6.solve();
 }
---
+++ lib/day_6/day_6.dart
@@ -0,0 +1,33 @@
+import 'package:advent_of_code_2022/data.dart';
+
+class Day6 {
+  static void solve() {
+    print("--- Day 6: Tuning Trouble ---");
+    List<String> message =
+        PrepareData.getData(path: "lib/day_6/day_6.txt", pattern: "");
+    part1(message);
+  }
+
+  static int markerLen = 4;
+
+  static void part1(List<String> message) {
+    print("Part 1:");
+    int result = getMarkerIndex(message, markerLen);
+    print(
+        "The characters need to be processed before the first start-of-packet marker is detected : $result");
+  }
+
+  static int getMarkerIndex(List<String> message, int key) {
+    for (int i = 0; i < message.length - key; i++) {
+      List<String> checkPart = message.sublist(i, key + i);
+      bool isMarker = markerChecker(checkPart, key);
+      if (isMarker) {
+        return key + i;
+      }
+    }
+    return 0;
+  }
+
+  static bool markerChecker(List<String> messagePart, int key) =>
+      messagePart.toSet().length == key;
+}
